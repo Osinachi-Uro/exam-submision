06ac1224be47a4d9f4433d20557aa9489bda2a7a
Osinachi Ibiam-Uro <83463641+Osinachi-Uro@users.noreply.github.com>
2023-03-19 21:05:38 +0100

    Delete SampleApp/kube-prometheus directory

--- SampleApp/kube-prometheus/helm.tf
+++
@@ -1,7 +0,0 @@
-resource "helm_release" "kube-prometheus" {
-  name       = "kube-prometheus-stacker"
-  namespace  = var.namespace
-  version    = "45.0.0"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-}
