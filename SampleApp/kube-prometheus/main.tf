5120f6fe86ea14df5e6640c1d992734e533700b6
Osinachi Ibiam-Uro <83463641+Osinachi-Uro@users.noreply.github.com>
2023-03-19 16:57:18 +0100

    Create main.tf

---
+++ SampleApp/kube-prometheus/main.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "kube-prometheus" {
+  name       = "kube-prometheus-stacker"
+  namespace  = var.namespace
+  version    = "45.0.0"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+}
