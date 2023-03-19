7f54bf01d504917ab3d05397c700a70d41fe3256
Osinachi Ibiam-Uro <83463641+Osinachi-Uro@users.noreply.github.com>
2023-03-19 20:07:43 +0100

    Create eks-cluster.tf

---
+++ SampleApp/terraform/eks-cluster.tf
@@ -0,0 +1,27 @@
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name = "AltschoolExam-eks-app-cluster"
+    cluster_version = "1.24"
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.AltschoolExam-vpc.vpc_id
+    subnet_ids = module.AltschoolExam-vpc.private_subnets
+
+    tags = {
+        environment = "development"
+        application = "AltschoolExam"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
+            max_size = 2
+            desired_size = 2
+
+            instance_types = ["t3.medium"]
+        }
+    }
+}
+
