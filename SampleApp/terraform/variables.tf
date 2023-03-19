8d12151fad3362a579554c28db3dd39147e729ab
Osinachi Ibiam-Uro <83463641+Osinachi-Uro@users.noreply.github.com>
2023-03-19 20:57:18 +0100

    Update variables.tf

--- SampleApp/terraform/variables.tf
+++ SampleApp/terraform/variables.tf
@@ -1,3 +1,16 @@
 variable "vpc_cidr_block" {}
 variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
+
+# Variables for the Helm-kubernetes module
+variable "kube_config" {
+  type    = string
+  default = "~/.kube/config"
+  description = "path to kubeconfig file"
+}
+
+variable "namespace" {
+  type    = string
+  default = "monitoring"
+  description = "value for namespace"
+}
